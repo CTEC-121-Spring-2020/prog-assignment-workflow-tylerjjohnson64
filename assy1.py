95b932bdef26e2625c821f65ad2d184f3dc34f43
unknown <t.johnson57@students.clark.edugit config --global user.email t.johnson57@students.clark.edu>
2020-04-22 15:27:42 -0700

    tested and completed

---
+++ assy1.py
@@ -0,0 +1,14 @@
+""""
+Tyler Johnson
+Assignment 1 
+print statements
+"""
+
+"""
+Process: print statements of my choice to screen
+"""
+def main ():
+    print("hello")
+    print("my name is Tyler")
+    print("I am using python")
+main()
